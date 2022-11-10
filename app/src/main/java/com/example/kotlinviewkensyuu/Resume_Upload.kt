0729feaeff52e9b120f74bc5e0be8f5338092c3b
anki <5477ann5477@gmail.com>
2022-11-10 09:37:14 +0900

    履歴書アップロード画面を追加しました

---
+++ app/src/main/java/com/example/kotlinviewkensyuu/Resume_Upload.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinviewkensyuu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Resume_Upload : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resume_upload)
+    }
+}
