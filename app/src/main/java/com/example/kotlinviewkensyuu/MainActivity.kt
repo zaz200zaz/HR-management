4b63e058b2953514759c0852b2c9a535c9175be3
tuan <tranthanhtuan199981@gmail.com>
2022-11-10 17:36:56 +0900

    login機能と新規登録機能

--- app/src/main/java/com/example/kotlinviewkensyuu/MainActivity.kt
+++ app/src/main/java/com/example/kotlinviewkensyuu/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.kotlinviewkensyuu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        //login機能
+        btnLogin.setOnClickListener(View.OnClickListener {
+            loginHandle()
+        })
+
+        //新規登録機能
+        gotoRegister.setOnClickListener(View.OnClickListener {
+
+        })
+
+    }
+    fun loginHandle(){
+        var email : String = inputEmail.text.toString()
+        var paswords : String = inputEmail.text.toString()
+        if (email=="admin@gmail.com"&&paswords == "123456"){
+
+            Toast.makeText(this,"ログイン成功",Toast.LENGTH_LONG).show()
+        }
+    }
+    fun singUp(){
+        val intern = Intent(this,Register::class.java)
+        startActivity(intern)
     }
 }
