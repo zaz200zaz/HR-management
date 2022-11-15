625eb0551dd4ecbe0637c591e6f568cb10e9b87d
tuan <tranthanhtuan199981@gmail.com>
2022-11-15 15:22:38 +0900

    チャンタントゥアン最新版エラーなし

--- app/src/main/java/com/example/kotlinviewkensyuu/Register.kt
+++ app/src/main/java/com/example/kotlinviewkensyuu/Register.kt
@@ -1,32 +1,46 @@
 package com.example.kotlinviewkensyuu
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var refUsers: DatabaseReference
+    private var FirebaseUserID: String = ""
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mAuth = FirebaseAuth.getInstance()
         AlreadyHaveAccount.setOnClickListener(View.OnClickListener {
             startActivity(Intent(this,MainActivity::class.java))
         })
-        btnRegister.setOnClickListener(View.OnClickListener {
+        btnRegister.setOnClickListener(View.OnClickListener
+        {
+
             check(inputEmail.text.toString().trim(),InputConfirmEmail.text.toString().trim(),
                 inputPasswords.text.toString().trim(),InputConfirmPassword.text.toString().trim(),)
-
         })
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun check(trim: String, trim1: String, trim2: String, trim3: String) {
         var checkEmail: Boolean = false
         var checkPassword: Boolean = false
@@ -69,11 +83,35 @@ class Register : AppCompatActivity() {
         }
         if (checkEmail && checkPassword){
 
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(trim,trim2).addOnCompleteListener(
-                OnCompleteListener {
 
+            mAuth.createUserWithEmailAndPassword(trim,trim2).addOnCompleteListener{task ->
+                if (task.isSuccessful){
+                    FirebaseUserID = mAuth.currentUser!!.uid
+                    refUsers = FirebaseDatabase.getInstance().reference.child("Users").child(FirebaseUserID)
 
-                })
-        }
+                    val userHasMap = HashMap<String, Any>()
+                    userHasMap["uid"] = FirebaseUserID
+                    userHasMap["email"] = trim
+                    userHasMap["password"] = trim2
+                    refUsers.updateChildren(userHasMap)
+                        .addOnCompleteListener{task ->
+                            if (task.isSuccessful){
+                                Toast.makeText(this,"登録成功",Toast.LENGTH_LONG).show()
+                                startActivity(Intent(this,MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK))
+                                finish()
+                            }
+                        }
+
+                    //hai
+                }else{
+                    Log.d(TAG, "check22: "+task.exception!!.message.toString())
+                    Toast.makeText(this,"エラー：" + task.exception!!.message.toString(),Toast.LENGTH_LONG).show()
+                }
+            }
+
+            }
+
     }
+
 }
+
